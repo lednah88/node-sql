d990e6cfcc585c75b119afda47d3c1830e8be034
lednah88 <lednah97@gmail.com>
2020-09-09 17:08:23 +0200

    added specs

--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,20 +1,34 @@
-let { createTable,addNewVisitor,listAllVisitors,deleteVisitor,updateVisitor,viewOneVisitor,deleteAllVisitors } = require("../src/app");
+let {
+  createTable,
+  addNewVisitor,
+  listAllVisitors,
+  deleteVisitor,
+  updateVisitor,
+  viewOneVisitor,
+  deleteAllVisitors,
+} = require("../src/app");
 
 describe("Visitor", () => {
   it("should check if createTable is defined", () => {
     expect(createTable).toBeDefined();
   });
-  it("should check if addNewVisitor is defined", () => {
+  it("should check if addNewVisitor exist", () => {
     expect(addNewVisitor).not.toBeUndefined();
   });
   it("should check if addNewVisitor is defined", () => {
     expect(addNewVisitor).toBeDefined();
+  });
+  it("should check if listAllVisitors is defined", () => {
+    expect(listAllVisitors).toBeDefined();
   });
   it("should check if deleteVisitor is not null", () => {
     expect(deleteVisitor).not.toBe(null);
   });
   it("should check if updateVisitor is defined", () => {
     expect(updateVisitor).toBeDefined();
+  });
+  it("should check if viewOneVisitor is defined", () => {
+    expect(viewOneVisitor).toBeDefined();
   });
   it("Should check if deleteAllVisitors is defined", () => {
     expect(deleteAllVisitors).toBeDefined();
